7699c8f879f6a9059c8eef1595b049d5194b565b
Adriele <adriele.dejprimo@gmail.com>
2024-02-14 17:57:06 -0300

    Adding the connection with knex and mysql

---
+++ backend/src/models/connection.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const knex = require('knex')({
+    client: 'mysql2',
+    connection:{
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DB
+    }
+});
+
+module.exports = knex;
